1ad8649b0cebae7b96e952a70b94596ba2f5484e
farfax <mfarfax@gmail.com>
2019-11-13 20:38:21 +0100

    fix1

--- ops/prepare.sh
+++ ops/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../
+pwd
 openssl aes-256-cbc -K $encrypted_5704401eb47b_key -iv $encrypted_5704401eb47b_iv -in secrets.tar.enc -out secrets.tar -d
 tar xvf secrets.tar
 mv cert.pfx WebDocs/cert.pfx
@@ -8,4 +8,3 @@ eval "$(ssh-agent -s)"
 chmod 600 ./$KEY_FILE
 echo -e "Host $SERVER_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./$KEY_FILE
-cd ops
